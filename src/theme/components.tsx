994deb104181eb9291ddd15746e9cf3ce8ea0b84
Aamir Alam <aamiralam1991@gmail.com>
2022-07-26 11:04:03 +0530

    range preselect range selection, range chart implementation (Condensed)

--- src/theme/components.tsx
+++ src/theme/components.tsx
@@ -27,30 +27,6 @@ export const ButtonText = styled.button`
   }
 `;
 
-// A button that triggers some onClick result, but looks like a link.
-export const LinkStyledButton = styled.button<{ disabled?: boolean }>`
-    border: none;
-    text-decoration: none;
-    background: none;
-
-    cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
-    color: ${({ theme, disabled }) => (disabled ? theme.text2 : theme.winterMainButton)};
-    font-weight: 500;
-
-    :hover {
-        text-decoration: ${({ disabled }) => (disabled ? null : 'underline')};
-    }
-
-    :focus {
-        outline: none;
-        text-decoration: ${({ disabled }) => (disabled ? null : 'underline')};
-    }
-
-    :active {
-        text-decoration: none;
-    }
-`
-
 export const CloseIcon = styled(X)<{ onClick: () => void }>`
   cursor: pointer;
 `;
